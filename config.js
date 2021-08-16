941f5760c9a480a05c56ff2a868681cf05672b2e
Harsh Sharma <harshsharma1611@gmail.com>
2021-08-16 10:51:23 +0530

    third commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "5e596d75d3022aa2d8f613fe7d283e46"
+    }
+}
+
+module.exports = constants;
